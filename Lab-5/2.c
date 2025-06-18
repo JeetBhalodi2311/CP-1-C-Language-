2b08a0364a22a7b3fdc52f77940074e44082997c
Jeettt_bhalodi__ <166577676+JeetBhalodi2311@users.noreply.github.com>
2025-06-18 12:25:20 +0530

    Add files via upload

---
+++ Lab-5/2.c
@@ -0,0 +1,16 @@
+// i) using single printf 
+// #include<stdio.h>
+// void main()
+// {
+//     printf("Address: Alay Vatika, Govardhan Chowk, Rajkot 360004");
+// }
+
+
+// ii) using multiple printf.
+#include<stdio.h>
+void main()
+{
+    printf("Address: Alay Vatika,");
+    printf("\nGovardhan Chowk,");
+    printf("\nRajkot 360004");
+}
